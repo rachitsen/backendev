bc5badd0648e0f07020a4f9055326f0ea9028934
Rachit <rachitsen15@gmail.com>
2025-03-14 09:20:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("hello World");
+  res.send("Hi There!!");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1> LOGIN FROM HERE</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`App  listening on port ${port}`);
+});
